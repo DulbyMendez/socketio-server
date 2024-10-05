5f41cd3cfad3f8d8d9574803cb48eb24b79bf34f
Dulby Mendez <dulby.mendez@gmail.com>
2024-10-05 18:44:26 -0500

    fixing server

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 //import express from 'express';
-const path = require('path');
-const express = require('express');
-const app = express();
+const path = require("path");
+const http = require("http");
+const express = require("express");
 
-app.set('port', process.env.PORT || 3000)
+const app = express();
+const server = http.createServer(app);
+
+app.set("port", process.env.PORT || 3000);
 
 //static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-//start the server
-const server = app.listen(app.get('port'), ()=>{
-    console.log('Server on port: ', app.get('port'));
-})
-
-const SocketIO = require('socket.io');
+const SocketIO = require("socket.io");
 const io = SocketIO(server);
 
 // websocket
-io.on('connection', (socket)=>{
-    socket.on('chat:message', (data)=>{
-        console.log('datos: ', data);
-        io.sockets.emit('chat:message', data);
-    })
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
 
-    socket.on('chat:typing', (data)=>{
-        socket.broadcast.emit('chat:typing', data);
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chat:message", (data) => {
+    console.log("datos: ", data);
+    io.sockets.emit("chat:message", data);
+  });
+
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
+});
+
+//start the server
+//app.listen(app.get("port"), () => {
+server.listen(app.get("port"), () => {
+  console.log("Server on port: ", app.get("port"));
+});
